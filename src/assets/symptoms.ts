44f041d4390a7cbf56be3d7e461134f82be6e642
gopalpal10 <gopalpal1209@gmail.com>
2023-02-09 23:03:22 +0530

    symptom data added to json

--- src/assets/symptoms.ts
+++ src/assets/symptoms.ts
@@ -12,14 +12,6 @@ export const Symptoms:any = [
         "description":"Nausea is an uneasiness of the stomach that often accompanies the urge to vomit, but doesn't always lead to vomiting"
     },
     {
-        "name": "Nausea1",
-        "description":"Nausea is an uneasiness of the stomach that often accompanies the urge to vomit, but doesn't always lead to vomiting"
-    },
-    {
-        "name": "Nausea2",
-        "description":"Nausea is an uneasiness of the stomach that often accompanies the urge to vomit, but doesn't always lead to vomiting"
-    },
-    {
         "name": "itching",
         "description":"Nausea is an uneasiness of the stomach that often accompanies the urge to vomit, but doesn't always lead to vomiting"
     },
@@ -36,7 +28,340 @@ export const Symptoms:any = [
         "description":"Nausea is an uneasiness of the stomach that often accompanies the urge to vomit, but doesn't always lead to vomiting"
     },
     {
-        "name": "Nausea3",
-        "description":"Nausea is an uneasiness of the stomach that often accompanies the urge to vomit, but doesn't always lead to vomiting"
-    }
+        "name":"skin rash",
+        "description":"A rash is irritated or swollen skin on your body. Rashes are often itchy and painful and can appear differently on different skin tones"
+    },
+    {
+        "name":"nodal skin eruptions",
+        "description":"A rash accompanied by swollen lymph nodes usually stems from a viral infection, such as measles, chickenpox, or mononucleosis."
+    },
+    
+    {
+        "name":"continuous sneezing",
+        "description":"Sneezing is caused by irritation to the mucous membranes of the nose or throat,it is a powerful involuntary expulsion of air"
+    },
+    {
+        "name":"shivering",
+        "description":"Shivering occurs most commonly when a person is cold. It is usually temporary and should stop once the individual warms up"
+    },
+    {
+        "name":"shivering",
+        "description":"Shivering occurs most commonly when a person is cold. It is usually temporary and should stop once the individual warms up"
+    },
+    {
+        "name":"chills",
+        "description":"Chills are body’s way of raising its core temperature,Cold temperatures, viruses, infections and other illnesses can bring on chills."
+    },
+    {
+        "name":"joint pain",
+        "description":"Physical discomfort where two or more bones meet to form a joint, ranging from mild to disabling."
+    },
+    {
+        "name":"ulcers on tongue",
+        "description":"Generally white lesions under the tongue, whenever you have to diagnose, it is important to get the medical history of the patient."
+    },
+    {
+        "name":"muscle wasting",
+        "description":"Muscle atrophy can occur due to malnutrition, age, genetics, a lack of physical activity or certain medical conditions"
+    },
+    {
+        "name":"vomiting",
+        "description":" Vomiting or throwing-up is forcing the contents of the stomach up through the food pipe and out of the mouth"
+    },
+    {
+        "name":"burning micturition",
+        "description":"Discomfort, pain or burning when urinating,That feeling is a telltale symptom of a  (UTI)"
+    },
+    {
+        "name":"spotting urination",
+        "description":"Blood in urine can have causes that aren't due to underlying disease,may be due to a problem in your kidneys or other parts of the urinary tract"
+    },
+    {
+        "name":"Fatigue",
+        "description":"Fatigue is a feeling of constant tiredness or weakness and can be physical, mental or a combination of both"
+    },
+    {
+        "name":"Weight gain",
+        "description":"Weight gain can involve an increase in muscle mass, fat deposits, excess fluids such as water or other factors."
+    },
+    {
+        "name":"Anxiety",
+        "description":"Anxiety is your body’s natural response to stress,it is a feeling of fear, dread, and uneasinesss"
+    },
+    {
+        "name":"cold hands and feets",
+        "description":"cold hands and feets is cause of red blood cells may not have enough hemoglobin to transport oxygen from your lungs to the rest of your body. "
+    },
+    {
+        "name":"mood swing",
+        "description":"A mood swing is a sudden or intense change in emotional state. During a mood swing, a person may quickly switch from feeling happy and sad"
+    },
+    {
+        "name":"weight loss",
+        "description":"weight loss is when you lose weight without changing your diet or exercise routine. It can be a sign of stress or a serious illness."
+    },
+    {
+        "name":"Restlessness",
+        "description":"Restlessness is feeling the need to constantly move, being unable to calm your mind, or a combination of the two."
+    },
+    {
+        "name":"lethargy",
+        "description":"Lethargy refers to a sense of fatigue and a lack of both mental and physical energy and motivation"
+    },
+    {
+        "name":"patches in throat",
+        "description":"throat patches are often a symptom of a viral or bacterial infection"
+    },
+    {
+        "name":"irregular sugar level",
+        "description":"irregular sugar level refers to blood sugar levels that go too low or too high. Diabetes is the best-known condition involving dysglycemia"
+    },
+    {
+        "name":"cough",
+        "description":"A cough is a natural reflex that is your body's way of removing irritants from your upper (throat) and lower (lungs) airways"
+    },
+    {
+        "name":"high fever",
+        "description":""
+    },
+    {
+        "name":"Sunken",
+        "description":"Sunken eyes are usually just a result of aging, dehydration, or insufficient sleep. But they could also be caused by a medical condition."
+    },
+    {
+        "name":"breathlessness",
+        "description":" breathlessness is the feeling that you can't get enough air into your lungs. It might feel like your chest is tight"
+    },
+    {
+        "name":"Sweating",
+        "description":"Sweating is the release of liquid from the body's sweat glands. This liquid contains salt is the release of liquid from the body's sweat glands. This liquid contains salt"
+    },
+    {
+        "name":"dehydration",
+        "description":"Dehydration happens when your body doesn't have as much water as it needs. Without enough, your body can't function properly"
+    },
+    {
+        "name":"indigestion",
+        "description":"indigestion is stomach pain with a burning sensation or fullness after eating. It's often related to stomach acid."
+    },
+    {
+        "name":"headache",
+        "description":" A headache is a pain in your head or face that's often described as a pressure that's throbbing, constant, sharp or dull."
+    },
+    {
+        "name":"yellow skin",
+        "description":"yellow skin is most commonly caused by a condition called jaundice, which occurs when there is a high level of bilirubin in the blood."
+    },
+    {
+        "name":"dark urine ",
+        "description":"Dark urine is usually a sign of dehydration. Dehydration occurs when there is not enough water in the body"
+    },
+    {
+        "name":"loss of appetite",
+        "description":"appetite occurs when you have a reduced desire to eat. It may also be known as a poor appetite or loss of appetite"
+    },
+    {
+        "name":"pain behind the eyes",
+        "description":"A feeling of pressure or pain behind your eyes doesn't always stem from a problem inside your eyes."
+    },
+    {
+        "name":"back pain ",
+        "description":"Back pain often happens because something is off in the way your spinal joints, muscles, discs, and nerves fit together and move"
+    },
+    {
+        "name":"constipation",
+        "description":"constipation occurs when bowel movements become less frequent and stools become difficult to pass"
+    },
+    {
+        "name":"abdominal pain",
+        "description":"Abdominal pain is pain that you feel anywhere between your chest and groin. This is often referred to as the stomach region or belly"
+    },
+    {
+        "name":"diarrhoea",
+        "description":"Diarrhea is the condition of having at least three loose, liquid, or watery bowel movements each day"
+    },
+    {
+        "name":"mild fever",
+        "description":"A body temp between 100.4 and 102.2 degree is usually considered a low-grade fever"
+    },
+    {
+        "name":"yellow urine ",
+        "description":"yellow urine caused by the presence of urobilin , a biochemical waste product generated from the breakdown of old red blood cells"
+    },
+    {
+        "name":"yellowing of eyes",
+        "description":" whites of your eyes might turn yellow when your body has too much of a chemical called bilirubin"
+    },
+    {
+        "name":"acute liver failure",
+        "description":"Acute liver failure (ALF) occurs when many cells in the liver die or become very damaged in a short amount of time."
+    },
+    {
+        "name":"fluid overload ",
+        "description":"fluid overload is a condition where you have too much fluid volume in your body"
+    },
+    {
+        "name":"swelling of stomach",
+        "description":""
+    },
+    {
+        "name":"Swollen lymph nodes ",
+        "description":"Swollen lymph nodes usually occur as a result of infection from bacteria or viruses"
+    },
+    {
+        "name":"malaise",
+        "description":"Malaise is when you feel a sense of unease or a lack of well-being. From viral infections to heart disease, many disorders can lead to it."
+    },
+    {
+        "name":"blurred and distorted vision",
+        "description":"Blurry vision is very common. A problem with any of the components of your eye,such as the cornea, retina, or optic nerve"
+    },
+    {
+        "name":"phlegm",
+        "description":"phlegm is mucus produced by the respiratory system, excluding that produced by the nasal passages"
+    },
+    {
+        "name":"throat irritation",
+        "description":"A sore throat is pain, scratchiness or irritation of the throat that often worsens when you swallow"
+    },
+    {
+        "name":"redness of eyes",
+        "description":"Eye redness is most often due to swollen or dilated blood vessels. This makes the surface of the eye look red"
+    },
+    {
+        "name":"sinus pressure",
+        "description":"sinuses that can cause them to get blocked and filled with fluid. It is usually caused by cold or allergies."
+    },
+    {
+        "name":"runny nose ",
+        "description":"A runny nose can be caused by anything that irritates or inflames the nasal tissues"
+    },
+    {
+        "name":"congestion",
+        "description":"Congestion is when your nose becomes stuffed up and inflamed. "
+    },
+    {
+        "name":"Chest pain",
+        "description":"Chest pain is a pain in any area of your chest. It may spread to other areas, down your arms,neck or jaw."
+    },
+    {
+        "name":"weakness in limbs",
+        "description":"Lower limb weakness can cause difficulty with walking and stair climbing"
+    },
+    {
+        "name":"fast heart rate",
+        "description":"Tachycardia (tak-ih-KAHR-dee-uh) is the medical term for a heart rate over 100 beats a minute."
+    },
+    {
+        "name":"pain during bowel movements",
+        "description":"Painful bowel movements have many potential causes.including constipation, diarrhea, and hemorrhoids "
+    },
+    {
+        "name":"pain in anal region",
+        "description":"Pain in anal region with occasional blood and mucus in feces. Pain during defecation"
+    },
+    {
+        "name":"bloody stool",
+        "description":"Rectal bleeding is a symptom of conditions like hemorrhoids, anal fissures, inflammatory bowel disease (IBD), ulcers and colorectal cancer. "
+    }, 
+    {
+        "name":"irritation in anus",
+        "description":""
+    },
+    {
+        "name":"neck pain ",
+        "description":"Neck pain is common but it's not usually a sign of arthritis or medical condition"
+    },
+    {
+        "name":"cramps",
+        "description":"cramp is a sudden and involuntary contraction of one or more of your muscles."
+    },
+    {
+        "name":"bruising",
+        "description":"A bruise forms when blood vessels under the skin break,Bruises change color as they heal"
+    },
+    {
+        "name":"obesity",
+        "description":"Obesity is a complex disease involving an excessive amount of body fat and mass index is 25 or greater"
+    },
+    {
+        "name":"swollen legs",
+        "description":" Swelling in the legs is caused by a buildup of fluid. It can also result from inflammation of soft tissue"
+    },
+    {
+        "name":"swollen blood vessels",
+        "description":"Vasculitis refers to a large group of diseases that cause inflammation of the blood vessels"
+    },
+    {
+        "name":"puffy face and eyes",
+        "description":"A puffy face may be a normal reaction to an allergy, part of being pregnant, or a side effect of taking steroids."
+    },
+    {
+        "name":"enlarged thyroid",
+        "description":"main symptom is an enlarged thyroid gland. The size may range from a single small nodule to a large mass at the front of the neck."
+    },
+    {
+        "name":"",
+        "description":"A diet lacking in calcium contributes to dry, brittle nail lack of folic acid and vitamin C"
+    },
+    {
+        "name":"swollen extremeties",
+        "description":" swelling extremeties caused by too much fluid trapped in the body's tissues."
+    },
+    {
+        "name":"excessive hunger",
+        "description":"Excessive hunger is a sign that your body needs more food. It's often a result of imbalanced hunger hormones"
+    },
+    {
+        "name":"excessive hunger",
+        "description":""
+    },
+    {
+        "name":"drying and tingling lips",
+        "description":""
+    },
+    {
+        "name":"slurred speech'",
+        "description":"Slurred speech is when you have trouble speaking, your words are slow or garbled, or your words run together."
+    },
+    {
+        "name":"knee pain",
+        "description":"The location and severity of knee pain may vary, depending on the cause of the problem."
+    },
+    {
+        "name":"hip joint pain",
+        "description":"it is usually caused by problems with muscles, ligaments, tendons and other soft tissues that surround your hip joint."
+    },
+    {
+        "name":"muscle weakness",
+        "description":"Muscle weakness happens when your full effort doesn't produce a normal muscle contraction or movement."
+    },
+    {
+        "name":"stiff neck",
+        "description":"Neck stiffness is almost always a temporary symptom of overusing your neck or sleeping in an unusual position."
+    },
+    {
+        "name":"",
+        "description":""
+    },
+    {
+        "name":"",
+        "description":""
+    },
+    {
+        "name":"",
+        "description":""
+    },
+    {
+        "name":"",
+        "description":""
+    },
+    {
+        "name":"",
+        "description":""
+    },
+
+
+
+
 ]
