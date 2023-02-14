c4d47984ea98f59e9b19853f5cfb8b970f0606da
Sourav Kumar <souravbarh2000@gmail.com>
2023-02-15 00:08:09 +0530

    property added

--- src/assets/disease.ts
+++ src/assets/disease.ts
@@ -3,246 +3,287 @@
         "name": "Fungal infection",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Allergy",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "GERD",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Chronic cholestasis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Drug Reaction",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Peptic ulcer diseae",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "AIDS",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Diabetes ",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Gastroenteritis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Bronchial Asthma",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hypertension ",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Migraine",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Cervical spondylosis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Paralysis (brain hemorrhage)",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Jaundice",
         "description": "Yellow skin caused by the build-up of bilirubin in the blood.Jaundice may occur if the liver can't efficiently process red blood cells as they break down. It's normal in healthy newborns and usually clears on its own. At other ages, it may signal infection or liver disease.Symptoms include yellowing of the skin and whites of the eyes.Some newborns may require light therapy for a day or so. In other cases, treatment involves addressing the underlying cause.",
         "common cause": "Jaundice can have causes that aren't due to underlying disease. Examples include Gilbert's syndrome (a benign condition) or medication side effects.",
-        "treatment": "Notice any yellow tint to the skin or eyes of an infant, child or adult@Have dark coloured urine and abdominal swelling or expansion"
+        "treatment": "Notice any yellow tint to the skin or eyes of an infant, child or adult@Have dark coloured urine and abdominal swelling or expansion",
+        "symptom":""
     },
     {
         "name": "Malaria",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Chicken pox",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Dengue",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Typhoid",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "hepatitis A",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hepatitis B",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hepatitis C",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hepatitis D",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hepatitis E",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Alcoholic hepatitis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Tuberculosis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Common Cold",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Pneumonia",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Dimorphic hemmorhoids(piles)",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Heart attack",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Varicose veins",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hypothyroidism",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hyperthyroidism",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Hypoglycemia",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Osteoarthristis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Arthritis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "(vertigo) Paroymsal  Positional Vertigo",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Acne",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Urinary tract infection",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Psoriasis",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     },
     {
         "name": "Impetigo",
         "description": "",
         "common cause": "",
-        "treatment": ""
+        "treatment": "",
+        "symptom":""
     }
 ]
