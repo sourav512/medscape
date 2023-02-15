97fdf697019d7735993532e8092798a20b54a674
gopalpal10 <gopalpal1209@gmail.com>
2023-02-16 00:15:35 +0530

    disease added

--- src/assets/disease.ts
+++ src/assets/disease.ts
@@ -1,289 +1,371 @@
 [
     {
         "name": "Fungal infection",
-        "description": "",
-        "common cause": "",
-        "treatment": "",
-        "symptom":""
+        "search" : "https://www.google.com/search?q=Fungal infection",
+        "description": "A fungus that invades the tissue can cause a disease that's confined to the skin, spreads into tissue, bones and organs or affects the whole body.Symptoms depend on the area affected, but can include skin rash or vaginal infection resulting in abnormal discharge.",
+        "common cause":"",
+        "treatment": "Treatments include antifungal medication.",
+        "symptom":"Symptoms depend on the area affected, but can include skin rash or vaginal infection resulting in abnormal discharge.",
+        "specialist" :""
     },
     {
         "name": "Allergy",
-        "description": "",
-        "common cause": "",
-        "treatment": "",
-        "symptom":""
+        "search" : "https://www.google.com/search?q=Allergy",
+        "description": "Allergy occurs when a person reacts to substances in the environment that are harmless to most people These substances are known as allergens and are found in dust mites, pets, pollen, insects, ticks, moulds, foods and some medications.",
+        "common cause": "a runny nose or sneezing @ pain or tenderness around your cheeks, eyes or forehead @ coughing, wheezing or breathlessness @ itchy skin or a raised rash (hives) @ diarrhoea @ feeling or being sick.",
+        "treatment": "Allergen avoidance-Your provider will help you take steps to identify and avoid your allergy triggers,Medications. Depending on your allergy, medications can help reduce your immune system reaction and ease symptoms,Immunotherapy,Emergency epinephrine.",
+        "symptom":"Itchy, watery eyes,Itchy nose,Sneezing,Runny nose,Rashes, Hives (a rash with raised red patches), Stomach cramps,Vomiting.",
+        "specialist" :""
     },
     {
-        "name": "GERD",
-        "description": "",
-        "common cause": "",
-        "treatment": "",
-        "symptom":""
+        "name": "Gastroesophageal reflux disease (GERD)",
+        "search" : "https://www.google.com/search?q=Gastroesophageal reflux disease (GERD)",
+        "description": "This is a chronic disease that occurs when stomach acid or bile flows into the food pipe and irritates the lining. Acid reflux and heartburn more than twice a week may indicate GERD.",
+        "common cause": "Smoking.@Eating large meals or eating late at night.@Eating certain foods (triggers) such as fatty or fried foods.@Drinking certain beverages, such as alcohol or coffee.@Taking certain medications, such as aspirin.",
+        "treatment": "Relief from lifestyle changes and over-the-counter medication is usually temporary. Stronger medication may be required.",
+        "symptom":"Symptoms include burning pain in the chest that usually occurs after eating and worsens when lying down.",
+        "specialist" :"Primary Care Provider (PCP)@Gastroenterologist@Paediatrician"
     },
     {
         "name": "Chronic cholestasis",
-        "description": "",
-        "common cause": "",
-        "treatment": "",
-        "symptom":""
+        "search" : "https://www.google.com/search?q=Chronic%20cholestasis",
+        "description": "Chronic cholestatic diseases, whether occurring in infancy, childhood or adulthood, are characterized by defective bile acid transport from the liver to the intestine, which is caused by primary damage to the biliary epithelium in most cases.",
+        "common cause": "Among the most common causes of cholestatic liver disease are primary biliary cirrhosis (PBC) and primary sclerosing cholangitis (PSC).",
+        "treatment": "Taking a prescription medicine called ursodiol (Actigall, Urso, Urso Forte). This medicine helps to lower the level of bile acids in your blood. Other medicines to relieve itching also may be an option.@Soaking itchy areas in cool or lukewarm water.",
+        "symptom":"This may cause symptoms of indigestion and diarrhea. As cholestasis goes on, these more immediate effects can lead to longer-term consequences. Bile accumulating in your blood can cause symptoms of illness, jaundice and severe itching.",
+        "specialist" :""
     },
     {
         "name": "Drug Reaction",
-        "description": "",
-        "common cause": "",
-        "treatment": "",
-        "symptom":""
+        "search" : "https://www.google.com/search?q=Drug%20Reaction",
+        "description": "A drug allergy is the reaction of the immune system to a medicine. Any medicine — nonprescription, prescription or herbal — can provoke a drug allergy. However, a drug allergy is more likely with certain medicines.",
+        "common cause": "A drug allergy occurs when your immune system mistakenly identifies a drug as a harmful substance, such as a virus or bacterium. Once your immune system detects a drug as a harmful substance, it develops an antibody specific to that drug.",
+        "treatment": "Withdrawal of the drug. If your provider determines that you have a drug allergy — or likely allergy — discontinuing the drug is the first step in treatment. ...@Antihistamines@Corticosteroids@Treatment of anaphylaxis",
+        "symptom":"The most common symptoms of drug allergy are hives, rash or fever",
+        "specialist" :"SPT and IDT are usually conducted by allergists and immunologists in specialist drug allergy clinics."
     },
     {
         "name": "Peptic ulcer diseae",
+        "search" : "https://www.google.com/search?q=Peptic%20ulcer%20diseae",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "AIDS",
+        "search" : "https://www.google.com/search?q=AIDS",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Diabetes ",
+        "search" : "https://www.google.com/search?q=Diabetes ",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Gastroenteritis",
+        "search" : "https://www.google.com/search?q=Gastroenteritis",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Bronchial Asthma",
+        "search" : "https://www.google.com/search?q=Bronchial%20Asthma",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hypertension ",
+        "search" : "https://www.google.com/search?q=Hypertension ",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Migraine",
+        "search" : "https://www.google.com/search?q=Migraine",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Cervical spondylosis",
+        "search" : "https://www.google.com/search?q=Cervical%20spondylosis",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Paralysis (brain hemorrhage)",
+        "search" : "https://www.google.com/search?q=Paralysis%20(brain%20hemorrhage)",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Jaundice",
+        "search" : "https://www.google.com/search?q=Jaundice",
         "description": "Yellow skin caused by the build-up of bilirubin in the blood.Jaundice may occur if the liver can't efficiently process red blood cells as they break down. It's normal in healthy newborns and usually clears on its own. At other ages, it may signal infection or liver disease.Symptoms include yellowing of the skin and whites of the eyes.Some newborns may require light therapy for a day or so. In other cases, treatment involves addressing the underlying cause.",
         "common cause": "Jaundice can have causes that aren't due to underlying disease. Examples include Gilbert's syndrome (a benign condition) or medication side effects.",
         "treatment": "Notice any yellow tint to the skin or eyes of an infant, child or adult@Have dark coloured urine and abdominal swelling or expansion",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Malaria",
+        "search" : "https://www.google.com/search?q=Malaria",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Chicken pox",
+        "search" : "https://www.google.com/search?q=Chickenpox",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Dengue",
+        "search" : "https://www.google.com/search?q=Dengue",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Typhoid",
+        "search" : "https://www.google.com/search?q=Typhoid",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "hepatitis A",
+        "search" : "https://www.google.com/search?q=hepatitis%20A",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hepatitis B",
+        "search" : "https://www.google.com/search?q=Hepatitis%20B",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hepatitis C",
+        "search" : "https://www.google.com/search?q=Hepatitis%20C",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hepatitis D",
+        "search" : "https://www.google.com/search?q=Hepatitis%20D",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hepatitis E",
+        "search" : "https://www.google.com/search?q=Hepatitis%20E",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Alcoholic hepatitis",
+        "search" : "https://www.google.com/search?q=Alcoholic%20hepatitis",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Tuberculosis",
+        "search" : "https://www.google.com/search?q=Tuberculosis",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Common Cold",
+        "search" : "https://www.google.com/search?q=Common%20Cold",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Pneumonia",
+        "search" : "https://www.google.com/search?q=Pneumonia",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Dimorphic hemmorhoids(piles)",
+        "search" : "https://www.google.com/search?q=Dimorphic%20hemmorhoids(piles)",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Heart attack",
+        "search" : "https://www.google.com/search?q=Heart%20attack",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Varicose veins",
+        "search" : "https://www.google.com/search?q=Varicose%20veins",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hypothyroidism",
+        "search" : "https://www.google.com/search?q=Hypothyroidism",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hyperthyroidism",
+        "search" : "https://www.google.com/search?q=Hyperthyroidism",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Hypoglycemia",
+        "search" : "https://www.google.com/search?q=Hypoglycemia",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Osteoarthristis",
+        "search" : "https://www.google.com/search?q=Osteoarthristis",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Arthritis",
+        "search" : "https://www.google.com/search?q=Arthritis",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "(vertigo) Paroymsal  Positional Vertigo",
+        "search" : "https://www.google.com/search?q=(vertigo)%20Paroymsal%20Positional%20Vertigo",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Acne",
+        "search" : "https://www.google.com/search?q=Acne",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Urinary tract infection",
+        "search" : "https://www.google.com/search?q=Urinary%20tract%20infection",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Psoriasis",
+        "search" : "https://www.google.com/search?q=Psoriasis",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     },
     {
         "name": "Impetigo",
+        "search" : "https://www.google.com/search?q=Impetigo",
         "description": "",
         "common cause": "",
         "treatment": "",
-        "symptom":""
+        "symptom":"",
+        "specialist" :""
     }
 ]
