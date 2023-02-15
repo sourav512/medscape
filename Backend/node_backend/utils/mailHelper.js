11f05db732696d653b2df2d470a493386e109b83
Rahul Gupta <rahulg.wappnet@gmail.com>
2023-02-15 22:42:50 +0530

    Changes made in nodemailer

--- Backend/node_backend/utils/mailHelper.js
+++ Backend/node_backend/utils/mailHelper.js
@@ -1,23 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const mailHelper = async (option) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER, // generated ethereal user
-      pass: process.env.SMTP_PASS, // generated ethereal password
+  var transporter = nodemailer.createTransport({
+    service:process.env.SERVICE,
+    host:process.env.SMTP,
+    secureConnection: false,
+    tls:{
+      rejectUnauthorized: false,
     },
+    port:465,
+    auth:{
+      user:process.env.USER,
+      pass:process.env.PASS
+    }
   });
 
   const message = {
-    from: "rahulguptaslg20@gmail.com", // sender address
-    to: option.email, // list of receivers
-    subject: option.subject, // Subject line
-    text: option.message, // plain text body
+    to: option.email, 
+    subject: option.subject, 
+    text: option.message, 
   };
 
-  // send mail with defined transport object
   await transporter.sendMail(message);
 };
 
