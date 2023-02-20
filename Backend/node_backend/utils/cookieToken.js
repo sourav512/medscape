d526c8c10c8363c747ffe10226daa2aa8dd3f0d3
Sourav Kumar <souravbarh2000@gmail.com>
2023-02-21 00:07:16 +0530

    guard service added

--- Backend/node_backend/utils/cookieToken.js
+++ Backend/node_backend/utils/cookieToken.js
@@ -5,7 +5,8 @@ const cookieToken = async(user,res) =>{
         expires : new Date(
             Date.now() + process.env.COOKIE_TIME * 24 * 60 *  60 * 1000 
         ),
-        httpOnly:true
+        httpOnly:true,
+        secure:true
     }
     user.password = undefined
     res.status(200).cookie("token",token,options).json({
